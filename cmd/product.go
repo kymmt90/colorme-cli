c9ff72344320167c0a6d5c1d08831cf7ce5d3d62
Kohei Yamamoto <kymmt90@gmail.com>
2023-04-13 10:25:55 +0900

    Refactor

--- cmd/product.go
+++ cmd/product.go
@@ -7,7 +7,9 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -20,6 +22,10 @@ Model Number: %s
 Price: ¥%d
 Description: %s
 `
+
+type ProductsResource struct {
+	Products []Product `json:"products"`
+}
 
 type Product struct {
 	ID          int    `json:"id"`
@@ -39,7 +45,11 @@ type Shop struct {
 }
 
 var (
-	productCmd = &cobra.Command{
+	client = &http.Client{
+		Timeout: 30 * time.Second,
+	}
+	productFields = []string{"id", "name", "stocks", "model_number", "sales_price", "expl"}
+	productCmd    = &cobra.Command{
 		Use:   "product",
 		Short: "Manage products",
 		Run: func(cmd *cobra.Command, args []string) {
@@ -48,7 +58,10 @@ var (
 				log.Fatal(err)
 			}
 
-			GetProducts(j)
+			err = GetProducts(j)
+			if err != nil {
+				log.Fatal(err)
+			}
 		},
 	}
 	outputAsJson bool
@@ -59,16 +72,86 @@ func init() {
 	productCmd.Flags().BoolVarP(&outputAsJson, "json", "j", false, "output as JSON")
 }
 
-func getShop() (*ShopResource, error) {
-	req, err := http.NewRequestWithContext(context.Background(), "GET", "https://api.shop-pro.jp/v1/shop", nil)
+func GetProducts(outputAsJson bool) error {
+	if accessToken == nil {
+		fmt.Fprintln(os.Stderr, "Set COLORME_ACCESS_TOKEN")
+		os.Exit(1)
+	}
+
+	u, err := buildProductsURL()
 	if err != nil {
-		return nil, fmt.Errorf("getShop: %w", err)
+		return fmt.Errorf("GetProducts: %w", err)
+	}
+
+	req, err := buildGetRequest(u, *accessToken)
+	if err != nil {
+		return fmt.Errorf("GetProducts: %w", err)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("GetProducts: %w", err)
+	}
+	defer res.Body.Close()
+
+	if outputAsJson {
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			return fmt.Errorf("GetProducts: %w", err)
+		}
+
+		fmt.Println(body)
+		return nil
+	}
+
+	shop, err := getShop()
+	if err != nil {
+		return fmt.Errorf("GetProducts: %w", err)
+	}
+
+	var products ProductsResource
+	err = json.NewDecoder(res.Body).Decode(&products)
+	if err != nil {
+		return fmt.Errorf("GetProducts: %w", err)
+	}
+
+	for i, v := range products.Products {
+		fmt.Printf(productTemplate+"\n", i+1, v.Name, v.Stocks, v.ModelNumber, v.Price, v.Description)
+		fmt.Printf("View this product on the shop: %s/?pid=%d\n", shop.Shop.URL, v.ID)
+	}
+
+	return nil
+}
+
+func buildProductsURL() (string, error) {
+	u, err := url.Parse("https://api.shop-pro.jp/v1/products?limit=30")
+	if err != nil {
+		return "", fmt.Errorf("buildProductsURL: %w", err)
+	}
+
+	q := u.Query()
+	q.Set("fields", strings.Join(productFields, ","))
+	u.RawQuery = q.Encode()
+
+	return u.String(), nil
+}
+
+func buildGetRequest(url string, token string) (*http.Request, error) {
+	req, err := http.NewRequestWithContext(context.Background(), "GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("buildGetRequest: %w", err)
 	}
 
 	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Authorization", "Bearer "+*accessToken)
-	client := http.Client{
-		Timeout: 30 * time.Second,
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	return req, nil
+}
+
+func getShop() (*ShopResource, error) {
+	req, err := buildGetRequest("https://api.shop-pro.jp/v1/shop", *accessToken)
+	if err != nil {
+		return nil, fmt.Errorf("getShop: %w", err)
 	}
 
 	res, err := client.Do(req)
@@ -88,61 +171,4 @@ func getShop() (*ShopResource, error) {
 	}
 
 	return &shop, nil
-}
-
-func GetProducts(outputAsJson bool) {
-	if accessToken == nil {
-		fmt.Fprintln(os.Stderr, "Set COLORME_ACCESS_TOKEN")
-		os.Exit(1)
-	}
-
-	req, err := http.NewRequest("GET", "https://api.shop-pro.jp/v1/products?limit=1", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Authorization", "Bearer "+*accessToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if outputAsJson {
-		fmt.Println(string(body))
-		return
-	}
-
-	shop, err := getShop()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var payload map[string]interface{}
-	if err := json.Unmarshal(body, &payload); err != nil {
-		log.Fatal(err)
-	}
-
-	productsJson, err := json.Marshal(payload["products"])
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var products []Product
-	if err := json.Unmarshal(productsJson, &products); err != nil {
-		log.Fatal(err)
-	}
-
-	for i, v := range products {
-		fmt.Printf(productTemplate+"\n", i+1, v.Name, v.Stocks, v.ModelNumber, v.Price, v.Description)
-		fmt.Printf("View this product on the shop: %s/?pid=%d\n", shop.Shop.URL, v.ID)
-	}
 }
